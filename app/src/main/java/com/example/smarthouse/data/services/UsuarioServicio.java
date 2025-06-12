46f3566cb0c6e9029c410d1586b7be1d892bd242
Ever-VC <ever.vasquezc27@gmail.com>
2025-06-11 20:49:22 -0600

    Fix: Arreglar definición de método de interfaz
    Se corrige la definición del método `loginConCorreo` en la interfaz `UsuarioServicio` para que sea pública implícitamente, de acuerdo con las convenciones de Java para interfaces.

--- app/src/main/java/com/example/smarthouse/data/services/UsuarioServicio.java
+++ app/src/main/java/com/example/smarthouse/data/services/UsuarioServicio.java
@@ -3,7 +3,7 @@ package com.example.smarthouse.data.services;
 import com.google.android.gms.tasks.Task;
 
 public interface UsuarioServicio {
-    public Task<Void> loginConCorreo(String correo, String contrasenna);
+    Task<Void> loginConCorreo(String correo, String contrasenna);
     Task<Void> loginConGoogle(String idToken);
     Task<Void> validarCuentaGoogleConToken(String idToken, String token);
 }
