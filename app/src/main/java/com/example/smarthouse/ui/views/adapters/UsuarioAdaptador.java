c71d963566162ae90fe6d73b03983a83da290477
Ever-VC <ever.vasquezc27@gmail.com>
2025-06-15 15:45:56 -0600

    feat: agregar diálogo de confirmación para eliminar usuario
    Se implementó un diálogo de confirmación `AlertDialog` antes de eliminar un usuario.
    Este diálogo informa al usuario que la acción es irreversible.
    
    Además, se agregó una validación para impedir que el usuario actualmente logueado se elimine a sí mismo. En caso de intento, se muestra un mensaje `Toast` informativo.

--- app/src/main/java/com/example/smarthouse/ui/views/adapters/UsuarioAdaptador.java
+++ app/src/main/java/com/example/smarthouse/ui/views/adapters/UsuarioAdaptador.java
@@ -1,12 +1,15 @@
 package com.example.smarthouse.ui.views.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentManager;
@@ -14,6 +17,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smarthouse.R;
 import com.example.smarthouse.data.models.Usuario;
+import com.example.smarthouse.data.services.UsuarioSerImplement;
+import com.example.smarthouse.data.services.UsuarioServicio;
+import com.example.smarthouse.ui.utils.UsuarioSesion;
 import com.example.smarthouse.ui.views.dialogs.GestionUsuariosDialog;
 import com.example.smarthouse.ui.views.fragments.GestionUsuariosFragment;
 import com.example.smarthouse.ui.views.functions.IUsuariosListener;
@@ -63,7 +69,22 @@ public class UsuarioAdaptador extends RecyclerView.Adapter<UsuarioAdaptador.Usua
         holder.btnEliminarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                AlertDialog.Builder mensajeDeConfirmacion = new AlertDialog.Builder(context);
+                mensajeDeConfirmacion.setTitle("¿Está seguro que desea eliminar el registro?");
+                mensajeDeConfirmacion.setMessage("Esta acción no puede revertirse, por lo tanto los cambios serán permanentes.");
+                mensajeDeConfirmacion.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        UsuarioServicio usuarioServicio = new UsuarioSerImplement();
+                        if (usuario.getId().equals(UsuarioSesion.obtenerUsuario().getId())) {
+                            Toast.makeText(context, "No se puede eliminar el usuario con el que ha iniciado sesión actualmente.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        usuarioServicio.eliminarUsuario(usuario.getId());
+                        Toast.makeText(context, "Usuario eliminado exitosamente.", Toast.LENGTH_SHORT).show();
+                        listener.actualizarListaUsuarios();
+                    }
+                }).setNegativeButton("No", null).show();
             }
         });
     }
